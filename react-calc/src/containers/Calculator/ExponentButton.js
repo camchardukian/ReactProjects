6337162f82c4372d6026ca7aa9779f6e269c8dd3
Cameron Chardukian <camchardukian@gmail.com>
2020-03-14 18:34:17 +0700

    react-calc/feature/exponent-logic

--- react-calc/src/containers/Calculator/ExponentButton.js
+++ react-calc/src/containers/Calculator/ExponentButton.js
@@ -1,12 +1,10 @@
-import React
-// { useContext } 
-from 'react';
-// import { NumberContext } from '../../providers/NumberProvider.js'
+import React, { useContext } from 'react';
+import { NumberContext } from '../../providers/NumberProvider.js'
 
 const ExponentButton = () => {
-  // const {  } = useContext(NumberContext);
+  const { handleChooseOperatorType } = useContext(NumberContext);
   return (
-  <button className="calculator-arithmetic-operator exponent-button">
+  <button className="calculator-arithmetic-operator exponent-button" onClick={() => handleChooseOperatorType('to the power of')}>
     <div>x</div><div>y</div>
   </button>
   );
